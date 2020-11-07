05b10984777763db912315a83a7fa39d1a3e1286
GL-SI2020 <nesrinebenoun@gmail.com>
2020-11-07 11:12:28 +0100

    first commit

---
+++ src/main/java/tekup/de/storebook/Project2020Application.java
@@ -0,0 +1,13 @@
+package tekup.de.storebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2020Application.class, args);
+	}
+
+}
